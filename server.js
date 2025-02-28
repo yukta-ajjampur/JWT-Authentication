34e4f838478293817c869fdb951fd08a8a4f0511
yukta ajjampur <yukta.ajjampur@yahoo.com>
2025-02-27 22:45:09 -0500

    Initial commit - JWT Authentication

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const { expressjwt: exjwt } = require("express-jwt");
+
+const app = express();
+
+const PORT = 3000;
+const secretkey = "JSONWEBTOKEN-ASSIGNMENT!";
+
+const users = [
+  {
+    id: 1,
+    username: "Yukta",
+    password: "1234",
+  },
+  {
+    id: 2,
+    username: "Ajjampur",
+    password: "5678",
+  },
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
+  next();
+});
+
+app.use(function (err, req, res, next) {
+  if (err.name == "UnauthorizedError") {
+    res.status(401).json({
+      success: false,
+      officialErr: err,
+      err: "Direct URL Access Not Allowed!",
+    });
+  } else {
+    next(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  for (let user of users) {
+    if (username == user.username && password == user.password) {
+      let token = jwt.sign({ id: user.id, username: username }, secretkey, {
+        expiresIn: "5d",
+      });
+      return res.json({
+        success: true,
+        err: null,
+        token,
+      });
+    }
+  }
+
+  res.status(401).json({
+    success: false,
+    token: null,
+    err: "Incorrect username or password",
+  });
+});
+
+app.get(
+  "/api/dashboard",
+  exjwt({ secret: secretkey, algorithms: ["HS256"] }),
+  (req, res) => {
+    res.json({
+      success: true,
+      myContent: "Secret content  that only logged in people can see!",
+    });
+  },
+);
+
+app.listen(PORT, () => {
+  console.log(`App is running on port: ${PORT}`);
+});
